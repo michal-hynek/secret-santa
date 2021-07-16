b9b1bed2ef89163a287e9d49d181e9b0b6c8a6a9
michal-hynek <hynek.michal@gmail.com>
2021-07-16 11:37:24 -0700

    Added dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 import apiRouter from './resources';
 
+dotenv.config();
 const app = express();
 
 app.use(apiRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const start = (): void => {
     app.listen(PORT, () => {
