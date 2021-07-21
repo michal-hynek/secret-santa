db513701f741a49dbe3d921ac72a7e7f67cb8d69
michal-hynek <hynek.michal@gmail.com>
2021-07-21 11:39:41 -0700

    Added sample response to GET /lists/random

--- src/resources/lists/lists.controller.ts
+++ src/resources/lists/lists.controller.ts
@@ -16,6 +16,23 @@ import { RequestHandler } from 'express';
  *                      "$ref": "#/components/schemas/ExchangeList"
  */
 export const getList: RequestHandler = async (_req, res) => {
-    // TODO: implementation
-    res.status(200).json([]);
+    // TODO: get the people from the database
+    const peopleInDb = [
+        { id: 1, firstName: 'Padma', lastName: 'Patil' },
+        { id: 2, firstName: 'Parvati', lastName: 'Patil' },
+        { id: 3, firstName: 'Dean', lastName: 'Thomas' },
+        { id: 4, firstName: 'Seamus', lastName: 'Finnigan' },
+        { id: 5, firstName: 'Ernie', lastName: 'Macmillan' },
+    ];
+
+    // TODO: implement random list generation
+    const randomList: any[] = [
+        { giver: peopleInDb[0], receiver: peopleInDb[3] },
+        { giver: peopleInDb[1], receiver: peopleInDb[2] },
+        { giver: peopleInDb[2], receiver: peopleInDb[4] },
+        { giver: peopleInDb[3], receiver: peopleInDb[0] },
+        { giver: peopleInDb[4], receiver: peopleInDb[1] },
+    ];
+
+    res.status(200).json(randomList);
 };
