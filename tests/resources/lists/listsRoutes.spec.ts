6f60b5aa581fcae5d82f40d108be544af71a0923
michal-hynek <hynek.michal@gmail.com>
2021-07-21 11:40:32 -0700

    Fixed failing test

--- tests/resources/lists/listsRoutes.spec.ts
+++ tests/resources/lists/listsRoutes.spec.ts
@@ -3,7 +3,7 @@ import { app } from '../../../src/server';
 
 describe('Lists controller routes', () => {
     it('mounts getRandomList endpoint', async () => {
-        const res = await request(app).get('/lists');
+        const res = await request(app).get('/lists/random');
 
         expect(res.status).toEqual(200);
     });
